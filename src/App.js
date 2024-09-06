4b258d23c1052abc7b2b2cbaaf529642232623e5
wohozo <wohozoinfotech@gmail.com>
2024-09-06 11:40:09 +0530

    Auth page only show UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { auth } from "./components/firebase"; // assuming you have this file for Firebase config
+import Playground from "./pages/Playground";
+import Dashboard from "./pages/Dashbord";
 import Login from "./pages/Login";
+
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Optionally, you can show a loader
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* If the user is logged in, navigate to dashboard. Otherwise, show login */}
+        <Route
+          path="/"
+          element={user ? <Navigate to="/dashboard" /> : <Login />}
+        />
+        <Route
+          path="/dashboard"
+          element={user ? <Dashboard /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/playground"
+          element={user ? <Playground /> : <Navigate to="/" />}
+        />
       </Routes>
     </Router>
   );
