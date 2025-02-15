e964b2dafdc018260661c77cff79b8c1ce3ab574
wohozo <wohozoinfotech@gmail.com>
2025-02-16 01:38:40 +0530

    cors error

--- backend/main.js
+++ backend/main.js
@@ -9,9 +9,21 @@ const app = express();
 const PORT = 6969;
 
 // Middleware
+const allowedOrigins = [
+  "http://localhost:3000", // Local Development
+  "https://wohozocreator-4n1qgruj0-nagarajas-projects.vercel.app", // Vercel Deployment
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Needed for cookies/authentication
   })
 );
 
